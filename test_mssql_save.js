c1b818cab48ee27632ecf2e42ffdb10f5e0ee455
DreamerStar123 <hpeak712@gmail.com>
2023-10-10 12:35:00 -0400

    test mssql

--- test_mssql_save.js
+++ test_mssql_save.js
@@ -4,7 +4,7 @@ const mssql_query = require('./modules/mssql_query');
 
 (async () => {
     // read mssql data
-    const query = fs.readFileSync('query/query.txt', 'utf-8');
+    const query = fs.readFileSync('query/test.sql', 'utf-8');
     const recordset = await mssql_query.getResultFromSQLServer(query);
     fs.writeFileSync('data/test.json', JSON.stringify(recordset));
 })();

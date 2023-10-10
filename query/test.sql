8b0f8ce72351e6c05d317e18b279e101e9074e3b
DreamerStar123 <hpeak712@gmail.com>
2023-10-10 14:14:08 -0400

    test.sql change

--- query/test.sql
+++ query/test.sql
@@ -16,12 +16,12 @@ FROM
 	LEFT OUTER JOIN PRODUCTION.dbo.Source Source ON Material_Req.Material_Req = Source.Material_Req
 	LEFT OUTER JOIN PRODUCTION.dbo.PO_Detail PO_Detail ON Source.PO_Detail = PO_Detail.PO_Detail
 	LEFT OUTER JOIN PRODUCTION.dbo.PO_Header PO_Header ON PO_Detail.PO = PO_Header.PO
--- WHERE
--- 	Job.Status = 'Active' 
--- 	AND Material_Req.Type = 'R' 
--- 	AND Job.Job NOT LIKE 'T%' 
+WHERE
+	Job.Status = 'Active' 
+	AND Material_Req.Type = 'R' 
+	AND Job.Job NOT LIKE 'T%' 
 -- 	AND PO_Detail.PO_Type = 0 
-	-- AND CAST(Job.Last_Updated AS DATE) = CAST(GETDATE() - 1 AS DATE)
+	AND CAST(Job.Last_Updated AS DATE) = CAST(GETDATE() - 1 AS DATE)
 ORDER BY
 	Job.Job,
 	PO_Detail.PO,

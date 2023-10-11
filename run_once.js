7c7156283944afebb84baeddaccfabccb0e1de63
DreamerStar123 <hpeak712@gmail.com>
2023-10-10 23:36:24 -0400

    run_once update

--- run_once.js
+++ run_once.js
@@ -16,9 +16,13 @@ const logger = winston.createLogger({
     const openJobBoardId = 5249505765;
     const noJobBoardId = 5252735219;
     const machineStatusBoardId = 5240959201;
+    const rawMaterialOrdersBoardId = 5293869955;
+    const openMachineBoardId = 5293870025;
 
     await open_job.addNewOpenJobData(openJobBoardId, logger);
     await machine_status.updateMachineStatus(machineStatusBoardId, logger);
+    await mat_order.addNewMatOrderData(rawMaterialOrdersBoardId, logger);
+    await open_machine.addNewOpenMachineData(openMachineBoardId, logger);
 
     const seconds = (performance.now() - startTime) / 1000;
     logger.info(`****************************** Elapsed time: ${seconds} seconds. ******************************`);

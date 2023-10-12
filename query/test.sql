d91ca081a98ce23fa858593db820e4f5202e259a
DreamerStar123 <hpeak712@gmail.com>
2023-10-11 20:33:04 -0400

    feedback

--- query/test.sql
+++ query/test.sql
@@ -18,7 +18,7 @@ WHERE
 	Job.Status = 'Active' 
 	AND Material_Req.Type = 'R' 
 	AND Job.Job NOT LIKE 'T%' 
-	-- AND PO_Detail.PO_Type = 0 
+	AND PO_Detail.PO_Type = 0 
 	-- AND CAST(Job.Last_Updated AS DATE) = CAST(GETDATE() - 1 AS DATE)
 ORDER BY
 	Job.Job,
--- test_mssql.js
+++ test_mssql.js
@@ -10,8 +10,9 @@ const headers = {
     // read mssql data
     const query = fs.readFileSync('query/test.sql', 'utf-8');
     try {
-        const res = await axios.post('http://localhost:3000', {headers});
-        console.log(res.data);
+        const res = await axios.post('http://184.168.31.101:3000', {query}, {headers});
+        // console.log(res);
+        fs.writeFileSync('data/test.json', JSON.stringify(res.data));
     } catch (err) {
         console.log(err);
     }

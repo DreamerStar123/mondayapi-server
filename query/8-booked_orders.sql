7c0863accc3a39603bf8f92588fb49c7136bb269
Dream is not only dream <145146800+DreamerStar123@users.noreply.github.com>
2023-12-03 18:44:27 -0800

    Update 8-booked_orders.sql

--- query/8-booked_orders.sql
+++ query/8-booked_orders.sql
@@ -1,4 +1,16 @@
-SELECT dbo.SO_Detail.Promised_Date, dbo.SO_Detail.Sales_Order, dbo.SO_Detail.SO_Line, dbo.SO_Detail.Material, dbo.SO_Detail.Order_Qty, dbo.SO_Detail.Shipped_Qty, dbo.Material_Location.On_Hand_Qty, dbo.SO_Detail.Order_Qty - dbo.SO_Detail.Shipped_Qty AS Delta, dbo.SO_Detail.Status, dbo.Job.Job
+SELECT 
+dbo.SO_Detail.Promised_Date,
+ dbo.SO_Detail.Sales_Order,
+  dbo.SO_Header.Order_Date,
+  dbo.SO_Detail.SO_Line, 
+  dbo.SO_Detail.Material, 
+  dbo.SO_Detail.Order_Qty,
+  dbo.SO_Detail.Shipped_Qty,
+  dbo.Material_Location.On_Hand_Qty, 
+  dbo.SO_Detail.Order_Qty - dbo.SO_Detail.Shipped_Qty AS Open_Qty, 
+  dbo.SO_Detail.Status, 
+  dbo.Job.Job
 FROM (dbo.SO_Detail LEFT JOIN dbo.Job ON dbo.SO_Detail.Job = dbo.Job.Job) LEFT JOIN dbo.Material_Location ON dbo.SO_Detail.Material = dbo.Material_Location.Material
+INNER JOIN dbo.SO_Header ON dbo.SO_Detail.Sales_Order = dbo.SO_Header.Sales_Order
 WHERE (((dbo.SO_Detail.Order_Qty - dbo.SO_Detail.Shipped_Qty) > 0) AND ((dbo.SO_Detail.Status) = 'open' OR (dbo.SO_Detail.Status) = 'backorder') AND ((dbo.Job.Job) Is Null))
 ORDER BY dbo.SO_Detail.Promised_Date, dbo.SO_Detail.Sales_Order, dbo.SO_Detail.SO_Line;

1e8c728df6314549011a3a20596f98b753aaa9a8
Dream is not only dream <145146800+DreamerStar123@users.noreply.github.com>
2023-12-03 18:24:39 -0800

    Update 8-booked_orders.sql

--- query/8-booked_orders.sql
+++ query/8-booked_orders.sql
@@ -1,21 +1,4 @@
-SELECT
-    dbo.SO_Detail.Sales_Order,
-    dbo.SO_Detail.SO_Line,
-    dbo.SO_Header.Order_Date,
-    dbo.SO_Detail.Material,
-    dbo.SO_Detail.Promised_Date,
-    dbo.SO_Detail.Status,
-    dbo.SO_Detail.Order_Qty,
-    dbo.SO_Detail.Shipped_Qty,
-    dbo.SO_Detail.Order_Qty - dbo.SO_Detail.Shipped_Qty AS Open_Qty
-FROM
-    dbo.SO_Detail
-    INNER JOIN dbo.SO_Header ON dbo.SO_Detail.Sales_Order = dbo.SO_Header.Sales_Order
-WHERE
-    (
-        dbo.SO_Detail.Status = 'open'
-        AND dbo.SO_Detail.Order_Qty - dbo.SO_Detail.Shipped_Qty <> 0
-    )
-    OR dbo.SO_Detail.Status = 'Backorder'
-ORDER BY
-    SO_Detail.Sales_Order;
+SELECT dbo.SO_Detail.Promised_Date, dbo.SO_Detail.Sales_Order, dbo.SO_Detail.SO_Line, dbo.SO_Detail.Material, dbo.SO_Detail.Order_Qty, dbo.SO_Detail.Shipped_Qty, dbo.Material_Location.On_Hand_Qty, dbo.SO_Detail.Order_Qty - dbo.SO_Detail.Shipped_Qty AS Delta, dbo.SO_Detail.Status, dbo.Job.Job
+FROM (dbo.SO_Detail LEFT JOIN dbo.Job ON dbo.SO_Detail.Job = dbo.Job.Job) LEFT JOIN dbo.Material_Location ON dbo.SO_Detail.Material = dbo.Material_Location.Material
+WHERE (((dbo.SO_Detail.Order_Qty - dbo.SO_Detail.Shipped_Qty) > 0) AND ((dbo.SO_Detail.Status) = 'open' OR (dbo.SO_Detail.Status) = 'backorder') AND ((dbo.Job.Job) Is Null))
+ORDER BY dbo.SO_Detail.Promised_Date, dbo.SO_Detail.Sales_Order, dbo.SO_Detail.SO_Line;
